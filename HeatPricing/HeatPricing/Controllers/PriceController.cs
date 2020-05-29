0122e4d97575004a9bb7df25d689c42b6e08a86e
jonas.hingebjerg@gmail.com <jonas.hingebjerg@gmail.com>
2020-05-29 11:24:19 +0200

    done

---
+++ HeatPricing/HeatPricing/Controllers/PriceController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectricityPricing.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+
+namespace HeatPricing.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PriceController : ControllerBase
+    {
+
+
+        private readonly ILogger<PriceController> _logger;
+        private readonly IPublicChargingService _publicChargingService;
+
+        public PriceController(
+            ILogger<PriceController> logger,
+            IPublicChargingService publicChargingService)
+        {
+            _logger = logger;
+            _publicChargingService = publicChargingService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromQuery] DateTime dateTime,
+            [FromQuery] double ressourceUsage,
+            [FromQuery] string unitOfMeassure)
+        {
+            var priceRequest = new PriceRequest()
+            {
+                DateTime = dateTime,
+                RessourceUsage = ressourceUsage,
+                UnitOfMeassure = unitOfMeassure
+            };
+
+            var totalPrice = await CalculateSubmissionPrice(priceRequest);
+
+            return Ok(totalPrice);
+        }
+
+        private async Task<SubmissionPrice> CalculateSubmissionPrice(PriceRequest priceRequest)
+        {
+            var heatPriceInfo = await _publicChargingService.GetHeatPriceForDate(priceRequest.DateTime);
+            var price = priceRequest.RessourceUsage * (heatPriceInfo.Price + heatPriceInfo.Tax);
+
+            var submissionPrice = new SubmissionPrice()
+            {
+                Currency = heatPriceInfo.Currency,
+                TotalCost = price,
+            };
+
+            return submissionPrice;
+        }
+    }
+}
---
+++ HeatPricing/HeatPricing/Services/PublicChargingService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ElectricityPricing.Services
+{
+    public class PublicChargingService : IPublicChargingService
+    {
+        private readonly string BaseUrl;
+        private readonly HttpClient _httpClient;
+
+        public PublicChargingService(
+            HttpClient httpClient,
+            IConfiguration config)
+        {
+            _httpClient = httpClient;
+            BaseUrl = config.GetValue<string>("BaseUrls:PublicChargingBaseUrl");
+        }
+
+        public async Task<PriceAndTaxes> GetHeatPriceForDate(DateTime dateTime)
+        {
+
+            var response = await _httpClient.GetAsync(CreateGetUrl(dateTime));
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var submissionPrice = JsonConvert.DeserializeObject<PriceAndTaxes>(responseBody);
+
+            return submissionPrice;
+
+        }
+
+        private string CreateGetUrl(DateTime dateTime)
+        {
+            var getUrl = BaseUrl + "? datetime = [DateTime]";
+            getUrl = getUrl.Replace("[DateTime]", dateTime.ToString());
+
+            return getUrl;
+        }
+    }
+}
